705fb1e8d3a04a872fe9ba125a1bb02fedf8176b
zhuicode99 <enlin291814833@hotmail.com>
2022-09-07 00:25:07 +0000

    before url shortening

--- express_server.js
+++ express_server.js
@@ -26,7 +26,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
 
 
 app.listen(PORT, () => {
