9b579d8ef4f166e8c12c0b3a43e0475e75b2a28e
zhuicode99 <enlin291814833@hotmail.com>
2022-09-07 01:11:06 +0000

    before url shortening 2

--- express_server.js
+++ express_server.js
@@ -4,10 +4,14 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+function generateRandomString() {}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.use(express.urlencoded({ extended: true }));//body-parser,from buffer to str so we can read
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -26,10 +30,21 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
+
+
 
 
 app.listen(PORT, () => {
