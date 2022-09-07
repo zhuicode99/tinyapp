64c89130edbb75141ef14dfa76a95e6312df34c9
zhuicode99 <enlin291814833@hotmail.com>
2022-09-07 04:14:33 +0000

    fixed a bug,finished w3d1core

--- express_server.js
+++ express_server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser")
 
 app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: true }));//body-parser,from buffer to str so we can read
+app.use(bodyParser.urlencoded({ extended: true }));//body-parser,from buffer to str so we can read
 
 function generateRandomString() {
   let randomStr = "";
@@ -39,12 +40,6 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.post("/urls", (req, res) => {
-  let randomID = generateRandomString();
-  urlDatabase[randomID] = req.body.longURL; 
-  res.redirect("/urls/${randomID}");
-});
-
 app.get("/urls/:id", (req, res) => {  //id is shortURL
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
@@ -55,6 +50,12 @@ app.get("/u/:id", (req, res) => { //id is shortURL
   res.redirect(longURL);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  let id = generateRandomString();
+  urlDatabase[id] = req.body.longURL; 
+  res.redirect(`/urls/${id}`);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
